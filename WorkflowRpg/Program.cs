50527842a2d7354eebdaba3b8de8fb408166bf6d
Benedict Hazel <bwhazel@gmail.com>
2015-05-29 01:45:34 +0100

    Add workflow RPG solution files.

---
+++ WorkflowRpg/Program.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="Program.cs" company="Benedict W. Hazel">
+//     Benedict W. Hazel, 2015
+// </copyright>
+// <author>Benedict W. Hazel</author>
+// <summary>
+//     Program: Class for the main program logic.
+// </summary>
+
+namespace BWHazel.Experiments.WorkflowRpg
+{
+    using System.Activities;
+    
+    /// <summary>
+    /// Main program logic.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Program entry point.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+            Activity game = new Game();
+            WorkflowInvoker.Invoke(game);
+        }
+    }
+}
---
+++ WorkflowRpg/YesNoConverter.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="YesNoConverter.cs" company="Benedict W. Hazel">
+//     Benedict W. Hazel, 2015
+// </copyright>
+// <author>Benedict W. Hazel</author>
+// <summary>
+//     YesNoConverter: Class to convert Yes and No to boolean values.
+// </summary>
+
+namespace BWHazel.Experiments.WorkflowRpg
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Converts Yes and No to boolean values.
+    /// </summary>
+    public static class YesNoConverter
+    {
+        /// <summary>
+        /// Converts a Yes or No to a boolean value.
+        /// </summary>
+        /// <param name="value">The input text.</param>
+        /// <returns><c>true</c> for Yes or <c>false</c> for No.</returns>
+        public static bool ToBoolean(string value)
+        {
+            switch (value.ToLower())
+            {
+                case "yes":
+                case "y":
+                    return true;
+                case "no":
+                case "n":
+                default:
+                    return false;
+            }
+        }
+    }
+}
